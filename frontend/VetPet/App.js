e40f4b2a326b0832b6f141834272b5226a9be79f
BeckyU96 <beckyu96@gmail.com>
2024-08-25 11:33:44 +0300

    Add necessary project files and configurations for VetPet Application

---
+++ frontend/VetPet/App.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomePage from './screens/HomePage';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import TitlesScreen from './components/TitlesScreen';
+import Login from './screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+// Prevent splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync();
+
+function App() {
+  const [fontsLoaded] = useFonts({
+    FredokaLight: require('./assets/fonts/Fredoka-Light.ttf'),
+    FredokaMedium: require('./assets/fonts/Fredoka-Medium.ttf'),
+    FredokaBold: require('./assets/fonts/Fredoka-Bold.ttf'),
+    FredokaRegular: require('./assets/fonts/Fredoka-Regular.ttf'),
+    FredokaSemibold: require('./assets/fonts/Fredoka-SemiBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name='Login'
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="TitlesScreen"
+          component={TitlesScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomePage}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
